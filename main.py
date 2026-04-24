26842d3675d9e2d7262b255e14134554f12bdd14
Caio Vitorino <caiovitric@gmail.com>
2026-04-23 22:14:15 -0300

    Update main
    Adiciona a lógica do front ao main.

--- main.py
+++ main.py
@@ -1,59 +1,62 @@
-from datetime import datetime
+from datetime import date, datetime
 import web_scraping as ws
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-def setup():
-    nome = input("Nome: ")
+app = Flask(__name__)
+CORS(app)
 
-    if len(nome) < 3 or not nome.replace(" ", "").isalpha():
-        ws.log("User/ERROR", "Nome inválido")
-        return
+class datahandler:
 
-    url = input("URL: ")
+    def __init__(self , data: dict):
 
-    if ws.validate_url(url):
-        return
+        self.__dict__.update(data)
 
-    xpath = None
-    manual_xpath = input("XPath manual? (S/n): ")
+    @app.route('/app/dados', methods=['POST'])
+    def receive_data():
 
-    if manual_xpath in ("n", "N"):
-        value = input("Value: ")
-    elif manual_xpath in ("s", "S"):
-        tag = input("Tag: ")
-        attribute = input("Attribute: ")
-        value = input("Value: ")
-        xpath = f"//{tag}[@{attribute}='{value}']"
-    else:
-        return
+        data_JSON = request.get_json()
+        data_instace = datahandler(**data_JSON)
+        data_instace.setup()
 
-    driver_name = "chrome"
-    rep_time = 1
+    def setup(self):
 
-    if not isinstance(rep_time, (int, float)):
-        ws.log("User/ERROR", "Tempo de atuação inválido")
-        return
+        xpath = None
 
-    delay = 10
+        if len(self.name) < 3 or not self.name.replace(" ", "").isalpha():
+            ws.log("User/ERROR", "Nome inválido")
+            return
+        
+        if ws.validate_url(self.url):
+            return
+        
+        if not self.tag  or not self.attribute:
+            self.tag = None
+            self.attribute = None  
+        else:
+            xpath = f"//{self.tag}[@{self.attribute}='{self.value}']"
 
-    if not isinstance(delay, (int, float)):
-        ws.log("User/ERROR", "Delay inválido")
-        return
+        if not isinstance(self.interval, (int, float)) or not self.interval:
+            ws.log("User/ERROR", "Tempo de medição inválido")
+            return
+        
+        if not isinstance(self.operation, (int, float)) or not self.operation:
+            ws.log("User/ERROR", "Tempo de operação inválido")
+            return
+        
+        ws.log("User/INFO", f"Usuário: {self.name}")
+        ws.log("User/INFO", f"URL: {self.url}")
 
-    refresh = True
+        if xpath:
+            ws.log("User/INFO", f"XPath: {xpath}")
+        else:
+            ws.log("User/INFO", f"Valor inicial: {self.value}")             
 
-    ws.log("User/INFO", f"Usuário: {nome}")
-    ws.log("User/INFO", f"URL: {url}")
+        ws.log("User/INFO", f"Driver selecionado: chrome")
+        ws.log("User/INFO", f"Tempo de operação: {self.operation}")
+        ws.log("User/INFO", f"Intervalo entre leituras: {self.interval}")
+        
+        ws.analise(self.url, "chrome", self.operation, self.interval, self.refresh, self.value, manual_xpath=xpath)     
 
-    if xpath:
-        ws.log("User/INFO", f"XPath: {xpath}")
-    else:
-        ws.log("User/INFO", f"Valor inicial: {value}")
-
-    ws.log("User/INFO", f"Driver selecionado: {driver_name}")
-    ws.log("User/INFO", f"Tempo de atuação: {rep_time}")
-    ws.log("User/INFO", f"Intervalo entre leituras: {delay}")
-
-    ws.analise(url, driver_name, rep_time, delay, refresh, value=None, manual_xpath=xpath)
-
-while True:
-    setup()
+if __name__ == '__main__':
+    app.run(debug=True) 
